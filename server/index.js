cf4aad15d0f67918b99a14c76b38602414c485f1
chehin <chehinjuan@gmail.com>
2022-05-09 20:43:22 -0300

    se quito require de los sp

--- server/index.js
+++ server/index.js
@@ -9,19 +9,19 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // middleware para corregir error CORS
-app.use(function (req, res, next) {
-  // modifico los headers para definir las siguientes cositas
-  // Websites a los que permitir conexion (* cualquiera peligroso)
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // accioness que permitiremos
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Headers que quiero permitir
-  res.setHeader('Access-Control-Allow-Headers', 'x-auth, Content-Type');
-  // Se pone a true si por ejemplo queremos cookies en las requests a
-  // la API seria así
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-  // Por ultimo pasamos de este middleware a la siguiente capa con next() 
-  next();
+app.use(function(req, res, next) {
+    // modifico los headers para definir las siguientes cositas
+    // Websites a los que permitir conexion (* cualquiera peligroso)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // accioness que permitiremos
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Headers que quiero permitir
+    res.setHeader('Access-Control-Allow-Headers', 'x-auth, Content-Type');
+    // Se pone a true si por ejemplo queremos cookies en las requests a
+    // la API seria así
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+    // Por ultimo pasamos de este middleware a la siguiente capa con next() 
+    next();
 });
 
 // Definimos la Base de Datos
@@ -39,22 +39,22 @@ const articulos = require('./app/controllers/articulo.controller');
 // el "force: true" borra todas las tablas y las crea de nuevo en cada ejecucion
 // aparte hay triggers en crudo los puse separados en @storedprocedures.js
 db.sequelize.sync({
-  force: true
+    force: true
 }).then(() => {
-  console.log('**** Dropado todo y Resync con { force: true } ****');
-  
-  // genero los triggers para actualizar cantidades automaticamente en la db
-  require('./app/models/@storedprocedures')(db, db.sequelize, db.Sequelize);
+    console.log('**** Dropado todo y Resync con { force: true } ****');
 
-  // aca lleno las tablas de las entidades básicas el orden es importante
-  // sin facturas y algunos articulos informáticos todos en cero unidades 
-  usuarios.init();
-  telefonos.init();
-  proveedores.init();
-  clientes.init();
-  rubros.init();
-  articulos.init();
-  console.log('**** Datos iniciales generados con exito ****');
+    // genero los triggers para actualizar cantidades automaticamente en la db
+    // require('./app/models/@storedprocedures')(db, db.sequelize, db.Sequelize);
+
+    // aca lleno las tablas de las entidades básicas el orden es importante
+    // sin facturas y algunos articulos informáticos todos en cero unidades 
+    usuarios.init();
+    telefonos.init();
+    proveedores.init();
+    clientes.init();
+    rubros.init();
+    articulos.init();
+    console.log('**** Datos iniciales generados con exito ****');
 });
 
 // cargamos todos los routes de la server app 
@@ -62,6 +62,6 @@ require('./app/routes/route.js')(app); //
 
 // Iniciamos el Server
 db.env.listenOn
-var server = app.listen(db.env.puerto, db.env.listenOn, function () {
-  console.log("Server corriendo en http://%s:%s", server.address().address, server.address().port)
+var server = app.listen(db.env.puerto, db.env.listenOn, function() {
+    console.log("Server corriendo en http://%s:%s", server.address().address, server.address().port)
 });
