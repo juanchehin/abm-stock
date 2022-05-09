e154117e2f51380c3942239e1ddbc4a086514c83
chehin <chehinjuan@gmail.com>
2022-05-09 20:42:35 -0300

    configuracion para mysql

--- server/app/configs/env.js
+++ server/app/configs/env.js
@@ -1,22 +1,22 @@
 // mis variables de ambiente 
 const env = {
-  // para el express
-  listenOn: 'localhost',
-  puerto: 3000,
-  // para el postgres
-  database: 'Stock',
-  usuario: 'postgres',
-  clave: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  dialect: 'postgres',
-  // http://docs.sequelizejs.com/manual/installation/usage.html
-  // pool de conexiones
-  max: 5,
-  min: 0,
-  idle: 30000,
-  acquire: 60000,
-  
+    // para el express
+    listenOn: 'localhost',
+    puerto: 3000,
+    // para el postgres
+    database: 'stock',
+    usuario: 'root',
+    clave: '',
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    // http://docs.sequelizejs.com/manual/installation/usage.html
+    // pool de conexiones
+    max: 5,
+    min: 0,
+    idle: 30000,
+    acquire: 60000,
+
 };
 
 module.exports = env;
